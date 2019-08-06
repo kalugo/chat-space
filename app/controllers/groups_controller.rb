a8901a6b6c51da1cc9cf521072183099e08e1758
kalugo <bbkb4a3@gmail.com>
2019-08-06 13:02:26 +0900

    create group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,24 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(create_params)
+    redirect_to controller: :messages, action: :index
   end
 
   def edit
+    @group = Group.find(params[:group_id])
   end
 
   def update
   end
-  
+
+  private
+  def create_params
+    params.require(:group).permit(:name)
+  end
+
 end
