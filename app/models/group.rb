f4c3b26411cf8493aa29f8eb08dad838facb15bd
kalugo <bbkb4a3@gmail.com>
2019-08-10 13:37:41 +0900

    replace group last message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   validates :name, presence: true
+
+  def show_last_message
+    if messages.last.present?
+      if messages.last.body.present?
+        messages.last.body
+      else
+        "画像あり"
+      end
+    else
+      "未投稿"
+    end
+  end
 end
