90042155e962684dc0876bace066bed4ce51f56c
kalugo <bbkb4a3@gmail.com>
2019-08-13 18:39:20 +0900

    add function auto update

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -3,12 +3,12 @@ $(document).on('turbolinks:load', function() {
     var html = 
     `<div class="message">
     <div class="message__header">
-    <span class="user-name">${message.name}</span>
+    <span class="user-name" data-message-id="${message.id}">${message.name}</span>
     <span class="post-time">${message.created_at}</span>
     </div>
     <div class="message__text">
     <p>${message.body}</p>
-    <img src=${message.image} >
+    <img src=${message.image} ></img>
     </div>
     </div>`
     return html
@@ -36,4 +36,39 @@ $(document).on('turbolinks:load', function() {
     $('.chat-space__body').animate({scrollTop: $(".chat-space__body")[0].scrollHeight});
     return false;
   });
+
+  var reloadMessages = function(){
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.user-name').last().data('message-id')
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log("OK")
+      if(messages.length !== 0){
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML += buildHTML(message);
+          return insertHTML;
+        });
+        var messages = $(".chat-space__body");
+        messages.append(insertHTML);
+        $('.chat-space__body').animate({scrollTop: $(".chat-space__body")[0].scrollHeight});
+      }
+      else{
+        console.log("nothing")
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  }
+  var pattern = new RegExp("groups/\\d\\d/messages")
+  if(document.URL.match(pattern)) {
+    setInterval(reloadMessages, 5000);
+  }
 });
+
